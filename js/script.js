0aaf7183fd32718b0bcb01e3db071d275668caf0
hudson12345 <hud.a.barreiro@gmail.com>
2024-05-08 10:36:36 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let canvasPong = document.getElementById('canvas')/*varial para manipular o elemento canvas atraves do id*/
 let quadro = canvasPong.getContext('2d')/*deixa o canvas pronto para receber desenhos 2D*/
-let bola = canvasPong.getContext('2d')
-quadro.fillStyle = "blue"/*Colocando uma cor na variavel quadro*/
+let placar = canvasPong.getContext('2d')
+placar.fillStyle = "green"
+quadro.fillStyle = "red"/*Colocando uma cor na variavel quadro*/
 
 let player1 = {
     px:80,
@@ -11,21 +12,23 @@ let player1 = {
 }
 
 let player2 = {
-    px:80,
+    px:1150,
     py:260,
     tx:30,
     ty:200,
 }
 
-bola.fillStyle = "Red"
+
+
 let bolinha = {
     px:620,
-    py:345,
+    py:360,
     tx:30,
     ty:30,
+    dir:8,
 }
 
-quadro.font = '20px arial'
+placar.font = '20px arial'
 let pts1 = 0
 let pts2 = 0
 
@@ -33,7 +36,28 @@ function draw(){
     quadro.fillRect(player1.px,player1.py,player1.tx,player1.ty)
     quadro.fillRect(player2.px,player2.py,player2.tx,player2.ty)
     quadro.fillRect(bolinha.px,bolinha.py,bolinha.tx,bolinha.ty)
-    quadro.fillText(`Pontos:  ${pts1}`,300,50)
-    quadro.fillText(`Pontos:  ${pts2}`,1000,50)
+    placar.fillText(`Player 1:  ${pts1}`,300,50)
+    placar.fillText(`Player 2:  ${pts2}`,900,50)
 }
-draw()
+
+document.addEventListener('keydown', function () {player1.py += 10})
+
+
+function moverbolinha() {
+    bolinha.px += bolinha.dir
+
+    if(bolinha.px > 1150) {
+        bolinha.dir *= -1
+    }
+    else if(bolinha.px < 80){
+        bolinha.dir *= -1
+    }
+}
+
+function main() {
+    quadro.clearRect(0,0,1280,720)//limpando todos os elementos e depois redesenhando
+    draw()
+    moverbolinha()
+}
+
+setInterval(main, 10)
